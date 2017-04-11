9c7463cd96987a79ddc01c10979b23851ebd4c5f
Gilles Lenfant <gilles.lenfant@gmail.com>
2017-04-11 18:10:32 +0200

    Optim

--- apps/polls/admin.py
+++ apps/polls/admin.py
@@ -14,10 +14,11 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question_text', 'pub_date')
     ordering = ('id',)
     fieldsets = (
-        (None, {'fields': ['question_text']}),
-        ('Date information', {'fields':('pub_date',), 'classes': 'collapse'})
+        (None, {'fields': ('question_text',)}),
+        ('Date information', {'fields': ('pub_date',), 'classes': 'collapse'})
     )
     inlines = (ChoiceInline,)
+
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
