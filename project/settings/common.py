4048e180ee415b3d2420dfc5015b845af48850e8
Gilles Lenfant <gilles.lenfant@gmail.com>
2017-04-13 17:27:09 +0200

    changed debug toolbar

--- project/settings/common.py
+++ project/settings/common.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(base_dir / 'var'/ 'db' / 'db.sqlite3')
+        'NAME': str(base_dir / 'var' / 'db' / 'db.sqlite3')
     }
 }
 
